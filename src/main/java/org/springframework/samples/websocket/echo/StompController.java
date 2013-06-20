018a3798b953d96d818886a18ead0a3e60d9b277
Rossen Stoyanchev <rstoyanchev@gopivotal.com>
2013-06-19 22:20:19 -0400

    Add @MessageExceptionHandler + @RequestMapping methods

--- src/main/java/org/springframework/samples/websocket/echo/StompController.java
+++ src/main/java/org/springframework/samples/websocket/echo/StompController.java
@@ -1,46 +1,74 @@
 package org.springframework.samples.websocket.echo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
 import org.springframework.messaging.annotation.MessageMapping;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.messaging.PubSubHeaders;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.messaging.PubSubChannelRegistry;
+import org.springframework.web.messaging.annotation.MessageExceptionHandler;
 import org.springframework.web.messaging.annotation.SubscribeEvent;
+import org.springframework.web.messaging.support.PubSubMessageBuilder;
+
 
 @Controller
 public class StompController {
 
+	private final MessageChannel<Message<?>> brokerChannel;
+
+	private final MessageChannel<Message<?>> clientChannel;
+
+
+	@Autowired
+	public StompController(PubSubChannelRegistry<Message<?>, MessageHandler<Message<?>>> channelRegistry) {
+		this.brokerChannel = channelRegistry.getMessageBrokerChannel();
+		this.clientChannel = channelRegistry.getClientOutputChannel();
+	}
+
 
 	@SubscribeEvent(value="/init")
-	public Message<?> handleSubscribe(MessageChannel<Message<?>> channel) throws Exception {
+	public Message<?> handleInit() {
 
-		PubSubHeaders headers = PubSubHeaders.create();
-		headers.setDestination("/topic/echo");
-		Message<String> message = MessageBuilder.fromPayloadAndHeaders("simulated echo", headers.toMessageHeaders()).build();
+		Message<String> message = PubSubMessageBuilder.withPayload("fake echo").destination("/topic/echo").build();
+		this.brokerChannel.send(message);
 
-		channel.send(message);
-
-//		channel.send("/topic/echo", Collections.singletonMap("c", "d"), MediaType.APPLICATION_JSON);
-
-//		subscription.reply("message2:some other kind of data");
-//		subscription.reply(Collections.singletonMap("a", "b"), MediaType.APPLICATION_JSON);
-
-		headers = PubSubHeaders.create();
-		headers.setDestination("/init");
-		message = MessageBuilder.fromPayloadAndHeaders("message 1: some data", headers.toMessageHeaders()).build();
-		return message;
+		return PubSubMessageBuilder.withPayload("init data").build();
 	}
 
 	@MessageMapping(value="/echo")
-	public void handleEcho(String text, MessageChannel<Message<?>> channel) throws Exception {
+	public void handleEchoMessage(String text) {
 
-		PubSubHeaders headers = PubSubHeaders.create();
-		headers.setDestination("/topic/echo");
-		Message<String> message = MessageBuilder.fromPayloadAndHeaders(
-				"Echoing: " + text, headers.toMessageHeaders()).build();
+		if (text.equals("exception")) {
+			throw new IllegalStateException();
+		}
 
-		channel.send(message);
+		Message<String> message = PubSubMessageBuilder.withPayload("Echo: " + text)
+				.destination("/topic/echo").build();
+
+		this.brokerChannel.send(message);
+	}
+
+	@MessageExceptionHandler
+	public void handle(IllegalStateException ex) {
+
+		Message<String> message = PubSubMessageBuilder.withPayload("Exception: " + ex.toString())
+				.destination("/error").build();
+
+		this.clientChannel.send(message);
+	}
+
+	@RequestMapping(value="/echo", method=RequestMethod.POST)
+	@ResponseBody
+	public void handleEcho(String text) {
+
+		Message<String> message = PubSubMessageBuilder.withPayload("Echo (HTTP POST): " + text)
+				.destination("/topic/echo").build();
+
+		this.brokerChannel.send(message);
 	}
 
 }
